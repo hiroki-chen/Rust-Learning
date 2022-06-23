a858a76fe75ed5a06959e808de818f23a9490202
Hiroki Aoyama <etyyuiope@gmail.com>
2022-06-23 11:43:37 +0800

    Refactored the code

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,18 +1,7 @@
 use utils::grep_utils;
-use termcolor::{StandardStream, ColorChoice};
 
 fn main() {
-  let args = grep_utils::parse_command_line().expect("Parse error!");
-  let filename =
-    args.get("filename")
-      .expect("Please specify the filename by --filename=[name]")
-      .as_ref().expect("Filename is empty!");
-
-  let mut stdout = StandardStream::stdout(ColorChoice::Always);
-
-  grep_utils::colorized_log(None, &mut stdout, &format!("The filename given is {}.", filename)).expect("OK");
-  let lines = grep_utils::read_file(&filename).expect("The file does not exist!");
-  grep_utils::colorized_log(None, &mut stdout, &format!("The content given is {:?}.", lines)).expect("OK");
-
-  println!("Final result : {:?}", grep_utils::search_file(&lines, &"syStem".to_string()));
+  if let Err(e) = grep_utils::run() {
+    panic!("{}", e);
+  }
 }
--- grep/utils/src/lib.rs
+++ grep/utils/src/lib.rs
@@ -2,6 +2,7 @@ pub mod grep_utils;
 
 #[cfg(test)]
 mod tests {
+  use std::io::BufRead;
   use crate::grep_utils;
 
   #[test]
@@ -17,5 +18,62 @@ mod tests {
     assert!(grep_utils::truncate_string(str3).is_err());
     assert!(grep_utils::truncate_string(str4).is_err());
     assert_eq!(str5, grep_utils::truncate_string(str5).unwrap().as_str());
+  }
+
+  #[test]
+  pub fn test_read_file() {
+    dbg!("We are at directory: {}", std::env::current_dir().unwrap().to_str().unwrap());
+
+    let filename = "../README.md";
+    assert!(grep_utils::read_file(&filename.to_string()).is_ok());
+  }
+
+  #[test]
+  #[should_panic(expected = "File does not exist!")]
+  pub fn test_read_file_panic() {
+    let filename = "./";
+    grep_utils::read_file(&filename.to_string()).expect("File does not exist!");
+  }
+
+  #[test]
+  pub fn test_search_file() {
+    // Test one: No environment variable is set.
+    std::env::remove_var("CASE_SENSITIVE");
+    let filename = "../README.md";
+    let mut total_line: usize = 0;
+    std::io::BufReader::new(
+      std::fs::File::open(filename)
+        .unwrap())
+      .lines()
+      .into_iter()
+      .for_each(|_| total_line += 1 );
+
+    let contents = grep_utils::read_file(&filename.to_string()).unwrap();
+    let query1 = "Rust";
+    let query2 = "rUST";
+    let query3 = "";
+    let query4 = "^.*$";    // wildcard match.
+    let query5 = ".*is.*";  // all lines containing 'is'
+    assert_eq!(grep_utils::search_file(&contents, &query1.to_string()).len(), 1);
+    assert_eq!(grep_utils::search_file(&contents, &query2.to_string()).len(), 0);
+    assert_eq!(grep_utils::search_file(&contents, &query3.to_string()).len(), total_line);
+    assert_eq!(grep_utils::search_file(&contents, &query4.to_string()).len(), total_line);
+    assert_eq!(grep_utils::search_file(&contents, &query5.to_string()).len(), 2);
+
+    // Test two: Environment variable is set to false.
+    std::env::set_var("CASE_SENSITIVE", "false");
+    assert_eq!(grep_utils::search_file(&contents, &query1.to_string()).len(), 1);
+    assert_eq!(grep_utils::search_file(&contents, &query2.to_string()).len(), 1);
+    assert_eq!(grep_utils::search_file(&contents, &query3.to_string()).len(), total_line);
+    assert_eq!(grep_utils::search_file(&contents, &query4.to_string()).len(), total_line);
+    assert_eq!(grep_utils::search_file(&contents, &query5.to_string()).len(), 2);
+
+    // Test three: Environment variable is set to true.
+    std::env::set_var("CASE_SENSITIVE", "true");
+    assert_eq!(grep_utils::search_file(&contents, &query1.to_string()).len(), 1);
+    assert_eq!(grep_utils::search_file(&contents, &query2.to_string()).len(), 0);
+    assert_eq!(grep_utils::search_file(&contents, &query3.to_string()).len(), total_line);
+    assert_eq!(grep_utils::search_file(&contents, &query4.to_string()).len(), total_line);
+    assert_eq!(grep_utils::search_file(&contents, &query5.to_string()).len(), 2);
   }
 }
